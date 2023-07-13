1588cfaa5770a9dc56fcb7f0e5f76428f8513ce7
martialartsforamerica <139417137+martialartsforamerica@users.noreply.github.com>
2023-07-12 22:34:35 -0400

    Delete studio-manager-draft directory

--- studio-manager-draft/database_files.php
+++
@@ -1,8 +0,0 @@
-<?php
-//connect mysql database
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "filesystem";
-$con = mysqli_connect($host, $user, $pass, $db) or die("Error " . mysqli_error($con));
-?>
